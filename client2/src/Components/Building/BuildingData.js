365a62b32ce04a60a7894a19ee33257902d30674
Jacob G <jrgrant560@gmail.com>
2023-12-21 11:57:19 -0500

    removed broken client folder; replaced with client2

---
+++ client2/src/Components/Building/BuildingData.js
@@ -0,0 +1,44 @@
+const BuildingData = [
+  {
+    id: 1,
+    buildingField: "BUILDING ADDRESS",
+    formID: "street",
+    placeholder: "street",
+    // className:"",
+    name: "street",
+    type: "text",
+    feedback: "street is required",
+  },
+
+  {
+    id: 2,
+    formID: "city",
+    placeholder: "city",
+    // className:"",
+    name: "city",
+    type: "text",
+    feedback: "city is required",
+  },
+
+  {
+    id: 3,
+    formID: "state",
+    placeholder: "NY",
+    // className:"",
+    name: "state",
+    type: "text",
+    // feedback:""
+  },
+
+  {
+    id: 4,
+    formID: "zip_code",
+    placeholder: "zip code",
+    // className:"",
+    name: "zip_code",
+    type: "text",
+    feedback: "zip code is required",
+  },
+];
+
+export default BuildingData;
