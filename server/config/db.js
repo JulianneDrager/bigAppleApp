007b978709056615048037150ddc2a5b4a440c6a
Julianne Drager <67062523+JulianneDrager@users.noreply.github.com>
2024-01-26 15:30:31 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const mongoURI =
-    "mongodb+srv://big-apple-app:q3GZWAz3v9Tl2QBE@cluster0.4wnzb2v.mongodb.net/";
+    "";
 
   try {
     const connect = await mongoose.connect(mongoURI);
